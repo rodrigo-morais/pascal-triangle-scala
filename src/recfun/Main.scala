57399c3e2c5e42c8e24a1367c3cb83efd6ffb0f0
Rodrigo Morais <morais.rm@gmail.com>
2015-12-30 18:00:14 -0200

    Creating a project to present the pascal triangle.

---
+++ src/recfun/Main.scala
@@ -0,0 +1,15 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+  
+  def pascal(c: Int, r: Int): Int =
+     1
+}
